87fbbb2a5b497485e58d551cccda7b794b90b02c
sadhuram09 <your_email@gmail.com>
2026-04-28 16:02:54 +0530

    Add Render deployment config

--- app.py
+++ app.py
@@ -534,5 +534,6 @@ def list_languages():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     debug_mode = os.getenv("DEBUG", "false").lower() == "true"
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
